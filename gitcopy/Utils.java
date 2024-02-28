e1875392c31beb82b060f3e0369660fd5d44215b
jerrickzhu <jerrickzhu@gmail.com>
2024-02-27 22:28:26 -0800

    Add new function convertFileToBytes

--- gitcopy/Utils.java
+++ gitcopy/Utils.java
@@ -1,6 +1,35 @@
 package gitcopy;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This class provides some functions considered as utilities for
+ * streamlining the build of gitcopy.
+ */
 public class Utils {
+
+  /**
+   * Converts the file into an array of bytes.
+   * 
+   * @param file
+   * @return byte[]
+   */
+  public static byte[] convertFileToBytes(File file) {
+    if (file.isFile()) {
+      try {
+        Path pathOfFile = file.toPath();
+        byte[] fileConvertedToBytes = Files.readAllBytes(pathOfFile);
+        return fileConvertedToBytes;
+      } catch (IOException exception) {
+        throw new IllegalArgumentException(exception.getMessage());
+      }
+    } else {
+      throw new IllegalArgumentException("This is not a file.");
+    }
+  }
 
   /**
    * Function that validates if the arguments (commands entered) are valid. Valid
