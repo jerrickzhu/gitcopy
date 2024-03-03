19438624d30229f15bf95013947ea3bc99315967
jerrickzhu <jerrickzhu@gmail.com>
2024-03-02 23:39:12 -0800

    Add sha1 method and createFileInCurrentDirectory method

--- gitcopy/Utils.java
+++ gitcopy/Utils.java
@@ -2,8 +2,14 @@ package gitcopy;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.List;
 
 /**
  * This class provides some functions considered as utilities for
@@ -50,5 +56,57 @@ public class Utils {
         valid = false;
     }
     return valid;
+  }
+
+  /**
+   * Concatenates file path components into a file object.
+   * 
+   * @param directoryPath
+   * @param filename
+   * @return
+   */
+  public static File createFileInCurrentDirectory(
+      String directoryPath, String... filename) {
+    Path constructPath = Paths.get(directoryPath, filename);
+    return constructPath.toFile();
+  }
+
+  /**
+   * Calculates the SHA-1 hash of the concatenation of the provided objects
+   * whether they are byte arrays or strings.
+   * Returns the hash as a hexadecimal string
+   * 
+   * @param vals
+   * @return
+   */
+  public static String sha1(Object... vals) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("SHA-1");
+      for (Object val : vals) {
+        if (val instanceof byte[]) {
+          md.update((byte[]) val);
+        } else if (val instanceof String) {
+          md.update(((String) val).getBytes(StandardCharsets.UTF_8));
+        } else {
+          throw new IllegalArgumentException("improper type to sha1");
+        }
+      }
+      Formatter result = new Formatter();
+      for (byte b : md.digest()) {
+        result.format("%02x", b);
+      }
+      return result.toString();
+    } catch (NoSuchAlgorithmException excp) {
+      throw new IllegalArgumentException("System does not support SHA-1");
+    }
+  }
+
+  /**
+   * Converts a List<Object> to an array and then calculates
+   * the SHA-1 hash of the concatenation of the elements in the list.
+   * Returns the hash as a hexadecimal string.
+   */
+  static String sha1(List<Object> vals) {
+    return sha1(vals.toArray(new Object[vals.size()]));
   }
 }
