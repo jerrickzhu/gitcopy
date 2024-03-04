0371edcdeee69431ee7098b5d12196e2cd40730f
jerrickzhu <jerrickzhu@gmail.com>
2024-03-03 16:28:07 -0800

    Create new methods: serialize, deserialize, writeSerializedObjectToFile, writeContentsToFile, validateFile, writeContentsToStream

--- gitcopy/Utils.java
+++ gitcopy/Utils.java
@@ -1,7 +1,13 @@
 package gitcopy;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -106,7 +112,62 @@ public class Utils {
    * the SHA-1 hash of the concatenation of the elements in the list.
    * Returns the hash as a hexadecimal string.
    */
-  static String sha1(List<Object> vals) {
+  public static String sha1(List<Object> vals) {
     return sha1(vals.toArray(new Object[vals.size()]));
+  }
+
+  public static void writeSerializedObjectToFile(File file, Serializable object) {
+    writeContentsToFile(file, serialize(object));
+  }
+
+  public static byte[] serialize(Serializable object) {
+    try {
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      ObjectOutputStream objectStream = new ObjectOutputStream(outputStream);
+      objectStream.writeObject(object);
+      objectStream.close();
+      return outputStream.toByteArray();
+    } catch (IOException exception) {
+      throw new Error("Error serializiing");
+    }
+  }
+
+  public static <T extends Serializable> T deserialize(File file,
+      Class<T> expectedClass) {
+    try {
+      ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+      T result = expectedClass.cast(in.readObject());
+      in.close();
+      return result;
+    } catch (IOException | ClassCastException
+        | ClassNotFoundException excp) {
+      throw new IllegalArgumentException(excp.getMessage());
+    }
+  }
+
+  public static void writeContentsToFile(File file, Object... content) {
+    try {
+      validateFile(file);
+      BufferedOutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(file.toPath()));
+      writeContentsToStream(outputStream, content);
+      outputStream.close();
+    } catch (IOException | ClassCastException exception) {
+      throw new IllegalArgumentException(
+          "There was an error in writing the contents to file: " + exception.getMessage());
+    }
+  }
+
+  private static void validateFile(File file) {
+    if (file.isDirectory()) {
+      throw new IllegalArgumentException("Ensure this is a file and not a directory");
+    }
+  }
+
+  private static void writeContentsToStream(BufferedOutputStream outputStream, Object... contents) throws IOException {
+    for (Object content : contents) {
+      byte[] bytes = (content instanceof byte[]) ? (byte[]) content
+          : content.toString().getBytes(StandardCharsets.UTF_8);
+      outputStream.write(bytes);
+    }
   }
 }
